6e3d89588db9184aee37934467edc3fc9bb27b12
DESKTOP-20POM8C\Pablo <pablo-pinheiro@hotmail.com>
2021-02-15 12:13:27 -0300

    como implementar codigo let evidanto a criacao de variaveis, higher order function e como implementar

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,8 +1,33 @@
 package br.com.alura.bytebank
 
+import br.com.alura.bytebank.modelo.Autenticavel
 import br.com.alura.bytebank.modelo.Endereco
+import br.com.alura.bytebank.modelo.SistemaInterno
 
 fun main() {
+
+    var endereco = Endereco(logradouro = "Rua Vergueiro", numero = 3185)
+    val enderecoMaiusculo = "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+    println(enderecoMaiusculo)
+
+    Endereco(logradouro = "Rua Vergueiro", numero = 3185)
+        .let{endereco ->
+            "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+//    }.let{ //podemos retornar explicitamente no codigo
+//        enderecoEmMaiusculo ->
+//        println(enderecoEmMaiusculo)
+            //ou podemos referenciar o retorno de uma forma mais resumida
+        }.let(::println)
+
+    var enderecosComComplemento =
+        listOf(Endereco(logradouro = "Rua Vergueiro", numero = 3185), Endereco(), Endereco(complemento = "Casa"))
+            .filter { endereco -> endereco.complemento.isNotEmpty() }
+
+    println(enderecosComComplemento)
+
+    listOf(Endereco(logradouro = "Rua Vergueiro", numero = 3185), Endereco(), Endereco(complemento = "Casa"))
+        .filter(predicate ={ endereco -> endereco.complemento.isNotEmpty()})
+        .let(block = ::println)
 
     //higher order function, quando temos uma funcao que recebe ou devolve outra funcao
     //nao e muito utilizado, mas e possivel
@@ -11,22 +36,27 @@ fun main() {
 
     }
 
-    "".let(::testeRecebeString)
-
-    1.let{
-        it
+    soma(1,5){
+        println(it)
     }
 
-    teste(1,{})
+    soma(2,9, resultado = (::println))
 
-    val testeFuncao: () -> Unit
-    Endereco().let {
-        it
+    var autenticavel = object : Autenticavel {
+        val senha = 1234
+        override fun autentica(senha: Int) = this.senha == senha
     }
+
+    SistemaInterno().entra(autenticavel, 1234, autenticado = {
+        println("Realizar o pagamento")
+    })
 
 }
 
-fun testeRecebeString(valor: String){}
+fun soma(a: Int, b: Int, resultado: (Int) -> Unit){
+    println("antes da soma")
+    //preciso executar o resultado
+    resultado(a+b)
+    println("Depois da soma")
 
-fun teste(teste: Int, bloco: () -> Unit) {}
-
+}
--- src/br/com/alura/bytebank/modelo/SistemaInterno.kt
+++ src/br/com/alura/bytebank/modelo/SistemaInterno.kt
@@ -2,12 +2,13 @@ package br.com.alura.bytebank.modelo
 
 class SistemaInterno {
 
-    fun entra(admin: Autenticavel, senha: Int){
+    fun entra(admin: Autenticavel, senha: Int, autenticado: () -> Unit = {} ){
         if(admin.autentica(senha)){
             println("Bem vindo ao Bytebank")
         } else {
             println("Falha na autenticação")
         }
     }
+
 
 }
